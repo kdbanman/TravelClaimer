3445e59a38e7e6e0c4ce54ff6b4e361d955283ad
kdbanman <kdbanman@ualberta.ca>
2015-02-01 23:09:07 -0700

    finish.

--- src/com/kirbybanman/travelclaimer/core/TravelClaimerApp.java
+++ src/com/kirbybanman/travelclaimer/core/TravelClaimerApp.java
@@ -1,44 +1,102 @@
 package com.kirbybanman.travelclaimer.core;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Date;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.kirbybanman.travelclaimer.interfaces.ModelMutator;
 import com.kirbybanman.travelclaimer.model.Claim;
 import com.kirbybanman.travelclaimer.model.ClaimsList;
 import com.kirbybanman.travelclaimer.model.Expense;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
 public class TravelClaimerApp extends Application {
 
+	private static final String MODEL_FILENAME = "claims_list.gson";
+	
 	private static ClaimsList claimsList;
 	
 	@Override
 	public void onCreate() {
+		//attempt to GSON deserialize from save file.
+		//will be null if invalid or nonexistent
 		
-		//TODO: attempt to GSON deserialize from save file.  will be null if invalid or nonexistent
+		claimsList = loadModel();
 		
-		if (TravelClaimerApp.claimsList == null) {
-			TravelClaimerApp.claimsList = new ClaimsList();
+		// if the file didn't exist, probably the app hasn't been opened.
+		// create a new claims list model
+		if (claimsList == null) {
+			claimsList = new ClaimsList();
 		}
 		
-		addFixtureData();
+		//addFixtureData();
 	}
 	
 	public ClaimsList getClaimsList() {
 		return claimsList;
 	}
 	
+	/**
+	 * Calls the mutate function on the passed ModelMutator.
+	 * Immediately saves afterward.
+	 * @param mutator Closure for safe model mutation
+	 */
 	public void mutateModel(ModelMutator mutator) {
-		// TODO should throw on bad save
 		mutator.mutate(claimsList);
 		saveModel();
-		
 	}
 	
+	/* 
+	 * Adapted from 
+	 * https://github.com/joshua2ua/lonelyTwitter
+	 * on 1 Feb 2015
+	 */
 	public void saveModel() {
-		//TODO GSON to/from json return bool and log on errrrrrrrrrbody in da club
-
+		Gson gson = new Gson();
+		
+		try {
+			FileOutputStream fos = openFileOutput(MODEL_FILENAME, Context.MODE_PRIVATE);
+			OutputStreamWriter writer = new OutputStreamWriter(fos);
+			gson.toJson(claimsList, writer);
+			writer.flush();
+			fos.close();
+		} catch (FileNotFoundException e) {
+			Log.w("IO error", "file not found");
+		} catch (IOException e) {
+			Log.e("IO error", "file read failed");
+		}
+	}
+	
+	/* 
+	 * Adapted from 
+	 * https://github.com/joshua2ua/lonelyTwitter
+	 * on 1 Feb 2015
+	 */
+	private ClaimsList loadModel() {
+		ClaimsList claimsList = null;
+		Gson gson = new Gson();
+		try {
+			FileInputStream fis = openFileInput(MODEL_FILENAME);
+			InputStreamReader reader = new InputStreamReader(fis);
+			
+			claimsList = gson.fromJson(reader, (new TypeToken<ClaimsList>() {}).getType());
+			
+			fis.close();
+		} catch (FileNotFoundException e) {
+			Log.w("IO error", "file not found");
+		} catch (IOException e) {
+			Log.e("IO error", "file read failed");
+		}
+		return claimsList;
 	}
 	
 	/**
